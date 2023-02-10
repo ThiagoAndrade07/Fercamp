7d358858cdc8a3744b5203115cdd571732e94d84
João Pedro Moreno <105954332+Moreno1337@users.noreply.github.com>
2023-02-09 22:39:54 -0300

    Products JavaScript Application
    Dinamically inserting products and products description to the products view.

---
+++ js/produtos.js
@@ -0,0 +1,104 @@
+// Função para "Console.log" mais fácil
+function $(log) {
+    console.log(log);
+}
+
+const sectionProdutos = document.querySelector('.produtos');
+
+const templateDoProdutoSemDescricao = `
+
+    <div class="container-produtos">
+        <div class="box-produtos">
+            <img src="../assets/img-produtos/serra-marmore.png">
+        </div>
+        <div class="texto-produtos">
+            <p>Serra Mármore Makita 1300W</p>
+        </div>
+    </div>
+
+`;
+
+async function fetchProdutos() {
+
+    try {
+
+        let produtos = await fetch('../produtos-fake.json');
+        let produtosConvertidos = await produtos.json();
+    
+        return produtosConvertidos;
+
+    } catch {
+        console.log('Não foi possível resgatar os produtos.');
+    }
+}
+
+async function listaDeProdutos() {
+    const lista = await fetchProdutos();
+
+    lista.produtos.forEach( produto => {
+
+        criaProdutoComDescricao(produto.titulo, produto.imagem);
+        
+        let botoesDescricao = document.querySelectorAll('[data-botao-descricao]');
+        botoesDescricao.forEach( botao => botao.addEventListener('click', () => mostraDescricao(botao, produto.titulo, produto.descricao)));
+
+    });
+
+}
+
+function mostraDescricao(botao, titulo, descricao) {
+
+    let cardDoProduto = botao.parentNode.parentNode;
+    cardDoProduto.classList.add('produtos-com-descricao');
+    cardDoProduto.innerHTML = '';
+
+    let h5 = document.createElement('h5');
+    h5.innerHTML = titulo;
+    let divDescricao = document.createElement('div');
+    divDescricao.classList.add('texto-descricao');
+
+    cardDoProduto.appendChild(h5);
+    cardDoProduto.appendChild(divDescricao);
+
+    descricao.forEach( elemento => {
+        let div = document.createElement('div');
+        let h6 = document.createElement('h6');
+
+        h6.innerHTML = elemento.titulo;
+
+        div.appendChild(h6);
+
+        elemento.texto.forEach( desc => {
+            let p = document.createElement('p');
+            p.innerHTML = desc;
+
+            div.appendChild(p);
+        });
+
+        divDescricao.appendChild(div);
+    });
+
+}
+
+function criaProdutoComDescricao(titulo, imagem) {
+    let templateDoProdutoComDescricao = `
+
+        <div class="container-produtos">
+            <div class="box-produtos">
+                <img src="${imagem}">
+            </div>
+            <div class="botao-produtos">
+                <p>${titulo}</p>
+                <button class="descricao-botao" data-botao-descricao>Descrição</button>
+            </div>
+        </div>
+
+    `;
+
+    let div = document.createElement('div');
+    div.innerHTML = templateDoProdutoComDescricao;
+
+    sectionProdutos.appendChild(div);
+}
+
+listaDeProdutos();
