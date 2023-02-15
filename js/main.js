60fb929e4fe30b8d008a7d5d00bb4933bb957a4a
ThiagoAndrade07 <120478086+ThiagoAndrade07@users.noreply.github.com>
2023-02-15 17:36:20 -0300

    Header Update / Main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-const segmentos = document.querySelectorAll('.slide-link');
+const segmentos = document.querySelectorAll('#slide-link');
 
 segmentos.forEach( segmento => {
     segmento.addEventListener('click', () => {
         localStorage.setItem('segmento', segmento.firstChild.innerHTML.toLowerCase().trim());
+    });
+});
+
+const segmentosCabecalho = document.querySelectorAll('#link-produtos');
+
+segmentosCabecalho.forEach( segmento => {
+    segmento.addEventListener('click', () => {
+        localStorage.setItem('segmento', segmento.innerHTML.toLowerCase().trim());
     });
 });
